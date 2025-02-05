f326e2da68c1cf84217d97afdf7f769e802916bf
pismatej811 <pismatej811@gmail.com>
2025-02-05 19:58:41 +0100

    Akadamia JS

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 function zacni() {
     alert("Klikni ok Pre zacatie hry");
-    var userInput = prompt("Zadaj K, P, alebo N:");
-    console.log(userInput);
+    var userInput = prompt("Zadaj K, P, alebo N:").toUpperCase();
+
+    if (!["K", "P", "N"].includes(userInput)) {
+        alert("Zadal si zly znak Zadaj K, P, alebo N");
+        return;
+    }
+
+    var vyber = ["K", "P", "N"];
+    var vyberpc = vyber[Math.floor(Math.random() * vyber.length)];
+
+    var vyherca = "";
+    if (userInput === vyberpc) {
+        vyherca = "Remiza";
+    }
+    else if (
+        (userInput === "K" && vyberpc === "N") ||
+        (userInput === "P" && vyberpc === "K") ||
+        (userInput === "N" && vyberpc === "P")
+    ) {
+        vyherca = "Ty";
+    } else {
+        vyherca = "PC";
+    }
+    alert("PC si vybral: " + vyberpc + vyherca);
 }
