bbdf0339db7f43b3db1018d1fc4f1c431a73de7f
Matej <pismatej811@gmail.com>
2025-02-11 12:06:17 +0100

    changes

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 function zacni() {
-    alert("Klikni ok Pre zacatie hry");
-    var userInput = prompt("Zadaj K, P, alebo N:").toUpperCase();
+    alert('Klikni ok Pre zacatie hry');
+    var userInput = prompt('Zadaj K, P, alebo N:').toUpperCase();
 
-    if (!["K", "P", "N"].includes(userInput)) {
-        alert("Zadal si zly znak Zadaj K, P, alebo N");
+    if (!['K', 'P', 'N'].includes(userInput)) {
+        alert('Zadal si zly znak Zadaj K, P, alebo N');
         return;
     }
 
-    var vyber = ["K", "P", "N"];
+    var vyber = ['K', 'P', 'N'];
     var vyberpc = vyber[Math.floor(Math.random() * vyber.length)];
 
-    var vyherca = "";
+    var vyherca = '';
     if (userInput === vyberpc) {
-        vyherca = "Remiza";
+        vyherca = 'Remiza';
     }
     else if (
-        (userInput === "K" && vyberpc === "N") ||
-        (userInput === "P" && vyberpc === "K") ||
-        (userInput === "N" && vyberpc === "P")
+        (userInput === 'K' && vyberpc === 'N') ||
+        (userInput === 'P' && vyberpc === 'K') ||
+        (userInput === 'N' && vyberpc === 'P')
     ) {
-        vyherca = "Ty";
+        vyherca = 'Ty';
     } else {
-        vyherca = "PC";
+        vyherca = 'PC';
     }
-    alert("PC si vybral: " + vyberpc + "\n" + vyherca);
+    alert('PC si vybral: ' + vyberpc + '\n' + vyherca);
 }
